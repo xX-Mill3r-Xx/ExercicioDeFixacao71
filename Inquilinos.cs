597025cdb9c3bee344f1fc27186712c70fa3cf2f
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-19 16:59:04 -0300

    Adicionar arquivos de projeto.

---
+++ Inquilinos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDeFixacao71
+{
+    class Inquilinos
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Inquilinos()
+        {
+        }
+
+        public Inquilinos(string name, string email, int room)
+        {
+            Name = name;
+            Email = email;
+            Room = room;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioDeFixacao71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int qteQuartos = int.Parse(Console.ReadLine());
+            Inquilinos[] inq = new Inquilinos[qteQuartos];
+            Console.WriteLine($"Informe os dados dos {qteQuartos} Inquilinos:");
+            for(int i = 0; i < qteQuartos; i++)
+            {
+                Console.WriteLine($"Rent #{i+1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                inq[i] = new Inquilinos { Name = name, Email = email, Room = room };
+            }
+
+            Console.WriteLine("Informações dos inquilinos: ");
+
+            Console.ReadLine();
+        }
+    }
+}
